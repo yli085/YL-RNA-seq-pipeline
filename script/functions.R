9d559e3ffc5dfa7a349a292a35d3957033791717
Yahui Li <61255999+yli085@users.noreply.github.com>
2022-07-12 10:19:23 -0700

    Update functions.R

--- script/functions.R
+++ script/functions.R
@@ -1,10 +1,49 @@
-## PCA_label
+# tpmFun ------------------------------------------------------------------
 
-## PCA_simple
+## tpmFun: functions to convert counts to tpm
 
-## makePaddedDataFrame
+# inset function
+tpm <- function(counts, lengths) {
+  rate <- counts / lengths
+  rate / sum(rate) * 1e6
+}
+
+# main function
+tpmFun <-function(df){
+  df %>%
+    pivot_longer(7:ncol(df), names_to="sample", values_to="cnt") %>%
+    group_by(sample) %>%
+    mutate(tpm=tpm(cnt, Length)) %>%
+    dplyr::select(-cnt) %>%
+    pivot_wider(names_from=sample, values_from=tpm)
+}
+
+# rpkmFun -----------------------------------------------------------------
+
+rpkm <- function(counts, lengths) {
+  rate <- counts / lengths
+  rate / sum(counts) * 1e9
+}
+
+rpkmFun <- function(df) {
+  df %>% 
+    pivot_longer(7:ncol(df), names_to="sample", values_to="cnt") %>%
+    group_by(sample) %>%
+    mutate(rpkm=rpkm(cnt, Length)) %>%
+    select(-cnt) %>%
+    pivot_wider(names_from=sample, values_from=rpkm)
+}
 
 
-## tpmFun
 
-## fpkmFun
+# makePaddedDataFrame ---------------------------------------------------
+
+# the function for making padded dataframe
+na.pad <- function(x,len){
+  x[1:len]
+}
+
+makePaddedDataFrame <- function(l,...){
+  maxlen <- max(sapply(l,length))
+  data.frame(lapply(l,na.pad,len=maxlen),...)
+}
